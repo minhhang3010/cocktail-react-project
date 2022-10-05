04958cf75d536e2f9c86ff6c4885bd5e044e8a8d
minhhang3010 <minhhang.nguyen0103@gmail.com>
2022-10-05 10:54:12 +0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat iste magni aliquid corrupti magnam consequatur mollitia aspernatur, et quisquam debitis, saepe qui eaque! 
+        Provident necessitatibus optio quisquam atque excepturi! Eum!</p>
+    </section>
+  )
+}
+
+export default About
